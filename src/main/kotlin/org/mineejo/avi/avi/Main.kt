0eeb8211e979ac0d1ce929a01a46aee621492922
HeadcrabJ <reymond315qq@gmail.com>
2022-09-17 12:12:30 -0700

    Modal example

--- src/main/kotlin/org/mineejo/avi/avi/Main.kt
+++ src/main/kotlin/org/mineejo/avi/avi/Main.kt
@@ -1,3 +1,13 @@
 package org.mineejo.avi.avi
 
-class Main {}
+import com.intellij.openapi.actionSystem.AnActionEvent
+import com.intellij.openapi.actionSystem.PlatformDataKeys
+import com.intellij.openapi.project.DumbAwareAction
+import com.intellij.openapi.ui.Messages
+
+class Main : DumbAwareAction() {
+    override fun actionPerformed(e: AnActionEvent) {
+        val project = e.getData(PlatformDataKeys.PROJECT)
+        Messages.showMessageDialog(project, "Hi", "Greet", Messages.getInformationIcon())
+    }
+}
