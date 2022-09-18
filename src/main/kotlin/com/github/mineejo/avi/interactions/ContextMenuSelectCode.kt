2cfbc79105feffa41b7fbaae6a10821adbf22995
MineEjo <evgenyshaimov@gmail.com>
2022-09-18 23:11:44 +0300

    Added interaction billet

---
+++ src/main/kotlin/com/github/mineejo/avi/interactions/ContextMenuSelectCode.kt
@@ -0,0 +1,11 @@
+package com.github.mineejo.avi.interactions
+
+import com.intellij.openapi.actionSystem.AnAction
+import com.intellij.openapi.actionSystem.AnActionEvent
+
+class ContextMenuSelectCode : AnAction() {
+    override fun actionPerformed(e: AnActionEvent) {
+        // !Todo: No action required, but if there is a code, put it here.
+        println("Test")
+    }
+}
