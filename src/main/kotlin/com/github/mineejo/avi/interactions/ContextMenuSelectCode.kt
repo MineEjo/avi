988947deea5be2d9613328d4ce0f2de1d8fd1042
MineEjo <evgenyshaimov@gmail.com>
2022-09-20 21:37:44 +0300

    A separate class was created for writing points

--- src/main/kotlin/com/github/mineejo/avi/interactions/ContextMenuSelectCode.kt
+++ src/main/kotlin/com/github/mineejo/avi/interactions/ContextMenuSelectCode.kt
@@ -1,11 +1,9 @@
 package com.github.mineejo.avi.interactions
 
-import com.github.mineejo.avi.settings.ProjectSettingsState
+import com.github.mineejo.avi.interactions.writers.DotsWriter
 import com.intellij.openapi.actionSystem.AnAction
 import com.intellij.openapi.actionSystem.AnActionEvent
 import com.intellij.openapi.actionSystem.CommonDataKeys
-import com.intellij.openapi.actionSystem.PlatformDataKeys
-import com.intellij.openapi.editor.Caret
 import com.intellij.openapi.editor.Editor
 import com.intellij.openapi.project.Project
 import org.jetbrains.annotations.NotNull
@@ -21,27 +19,5 @@ class ContextMenuSelectCode : AnAction() {
         e.presentation.isEnabledAndVisible = project != null && editor != null && editor.selectionModel.hasSelection()
     }
 
-    // !Todo: Transfer simple functions to return to notifications or turn off menus.
-    override fun actionPerformed(e: AnActionEvent) {
-        val editor: Editor = e.getRequiredData(CommonDataKeys.EDITOR)
-        val project: Project = e.getRequiredData(CommonDataKeys.PROJECT)
-        val fileName: String? = e.getData(PlatformDataKeys.VIRTUAL_FILE)?.name
-
-        val primaryCaret: Caret = editor.caretModel.primaryCaret
-        val start: Int = primaryCaret.selectionStart
-        val end: Int = primaryCaret.selectionEnd
-
-        if (primaryCaret.selectedText == null || fileName == null) return
-
-        val text: String = primaryCaret.selectedText ?: ""
-        val id: Int = fileName.hashCode() + start + end + text.hashCode()
-
-        val settings: ProjectSettingsState = ProjectSettingsState().getInstance(project)
-
-        if (settings.dots.contains(id)) return
-
-        settings.dots = settings.dots + arrayOf(id, fileName, arrayOf(start, end), text)
-
-        primaryCaret.removeSelection()
-    }
+    override fun actionPerformed(e: AnActionEvent) = DotsWriter(e).saveDots()
 }
---
+++ src/main/kotlin/com/github/mineejo/avi/interactions/writers/DotsWriter.kt
@@ -0,0 +1,41 @@
+package com.github.mineejo.avi.interactions.writers
+
+import com.github.mineejo.avi.settings.ProjectSettingsState
+import com.intellij.openapi.actionSystem.AnActionEvent
+import com.intellij.openapi.actionSystem.CommonDataKeys
+import com.intellij.openapi.actionSystem.PlatformDataKeys
+import com.intellij.openapi.editor.Caret
+import com.intellij.openapi.editor.Editor
+import com.intellij.openapi.project.Project
+import com.intellij.openapi.ui.Messages
+
+class DotsWriter(e: AnActionEvent) {
+    private val titleWindow: String = "Adaptive Visual Interaction (AVI)"
+    private val noSelectedText: String = "An invalid code area is selected!"
+    private val noFileName = "Unable to define the file and save the selection!"
+
+    private val editor: Editor = e.getRequiredData(CommonDataKeys.EDITOR)
+    private val project: Project = e.getRequiredData(CommonDataKeys.PROJECT)
+    private val fileName: String? = e.getData(PlatformDataKeys.VIRTUAL_FILE)?.name
+
+    private val primaryCaret: Caret = editor.caretModel.primaryCaret
+    private val start: Int = primaryCaret.selectionStart
+    private val end: Int = primaryCaret.selectionEnd
+
+    fun saveDots() {
+        if (primaryCaret.selectedText == null) return Messages.showErrorDialog(noSelectedText, titleWindow)
+        if (fileName == null) return Messages.showErrorDialog(noFileName, titleWindow)
+
+        val text: String = primaryCaret.selectedText ?: ""
+        val id: Int = fileName.hashCode() + start + end + text.hashCode()
+
+        val settings: ProjectSettingsState = ProjectSettingsState().getInstance(project)
+
+        // ID is not unique and is compiled from data, if ID is the same = duplicate
+        if (settings.dots.contains(id)) return
+
+        settings.dots = settings.dots + arrayOf(id, fileName, arrayOf(start, end), text)
+
+        primaryCaret.removeSelection()
+    }
+}
