aa27d020f0b6fbdccdeab90a2ee17da819d83a7f
MineEjo <evgenyshaimov@gmail.com>
2022-09-21 22:03:25 +0300

    Added data validation after launching the editor

--- src/main/kotlin/com/github/mineejo/avi/events/Initialization.kt
+++ src/main/kotlin/com/github/mineejo/avi/events/Initialization.kt
@@ -1,10 +1,13 @@
-package com.github.mineejo.avi
+package com.github.mineejo.avi.events
 
+import com.github.mineejo.avi.area.VisualAreasChecker
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.vcs.changes.shelf.ShelveChangesManager.PostStartupActivity
 
-class Initialize : PostStartupActivity() {
+class Initialization : PostStartupActivity() {
     override fun runActivity(project: Project) {
         println("Plugin loaded after the project :D")
+
+        VisualAreasChecker().validate(project)
     }
 }
