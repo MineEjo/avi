227ff63ca7926fe80b0fbb1ae18b9bb0f0f4d74f
MineEjo <evgenyshaimov@gmail.com>
2022-09-25 20:06:09 +0300

    Added area component class

--- src/main/kotlin/com/github/mineejo/avi/area/VisualArea.kt
+++ src/main/kotlin/com/github/mineejo/avi/area/VisualArea.kt
@@ -1,9 +1,21 @@
 package com.github.mineejo.avi.area
 
-open class VisualArea {
-	protected val keyId: String = "id: "
-	protected val keyFile: String = "file: "
-	protected val keyLines: String = "lines: "
-	protected val keyContent: String = "content: "
-	protected val keySeparator: String = "-"
+open class VisualArea(
+	areaId: String,
+	areaFile: String,
+	private val areaLines: String,
+	areaContent: String
+) : VisualAreaFields() {
+
+	private fun processLines(): IntRange {
+		val lines: String = areaLines.replace(keyLines, "")
+		val start: Int = lines.split(keySeparator)[0].toInt()
+		val end: Int = lines.split(keySeparator)[1].toInt()
+		return IntRange(start, end)
+	}
+
+	val id: String = areaId.replace(keyId, "")
+	val file: String = areaFile.replace(keyFile, "")
+	val lines: IntRange = processLines()
+	val content: String = areaContent.replace(keyContent, "")
 }
